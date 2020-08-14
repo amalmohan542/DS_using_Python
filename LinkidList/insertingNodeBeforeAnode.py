8fffb21cb9853dc2ce37700d94bdfc48df214c69
AMAL MOHAN K <amalmohan542@gmail.com>
2020-08-14 23:23:50 +0530

    Program to insert a node before another node Added

--- LinkidList/insertingNodeBeforeAnode.py
+++ LinkidList/insertingNodeBeforeAnode.py
@@ -5,30 +5,27 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head=None
-    
-    def insertAfter(self, prev_nodeData, new_data): 
+    def insertBefore(self,postNode,newData):
         temp=self.head
         if temp == None:
-            print('There aren\'t any nodes to insert After!')
+            print('There aren\'t any nodes to insert before!')
         else:
             while temp:
-                if prev_nodeData == temp.data:
+                if temp.next.data==postNode:
                     break
                 else:
                     temp=temp.next
-            newNode=Node(new_data)
+            
+            newNode=Node(newData)
             newNode.next=temp.next
             temp.next=newNode
-
-        
-
     def printList(self):
         temp=self.head
         while(temp):
             print(temp.data)
             temp=temp.next
 
-if __name__ == "__main__":
+if __name__=="__main__":
     llist=LinkedList()
     llist.head=Node(1)#Created Node1 with head pointer
     second=Node(2) #Created 2nd node with with value 2
@@ -39,5 +36,7 @@ if __name__ == "__main__":
     third.next=fourth #Linked third node next part to 4th node
     llist.printList() #Print all elements of LinkidList
     print()
-    llist.insertAfter(2,78)
+    llist.insertBefore(3,78)
     llist.printList() #Print all elements of LinkidList
+        
+
